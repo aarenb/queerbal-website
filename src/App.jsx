fbd6ff15e582c323d03587b319810c3257f61880
Aaren Bertilsson <ab226gw@student.lnu.se>
2025-08-10 11:43:53 +0200

    Add castle pictures

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ function App() {
         <hr width="100%"/>
 
         <p> Många queerpersoner känner inte att de kan vara sig själva på t.ex sin skolbal, och vi vill därför ge queerpersoner i Kalmar denna möjlighet. HBTQIA+ personer är en väldigt utsatt grupp som inte alltid blir representerade i övriga arrangemang. Vi vill kunna lyfta upp dem och skapa en magisk kväll där HBTQIA+ personer står i centrum, där de kan känna sig trygga och ha roligt. Under evenemanget kommer även tillgänglighet vara en faktor som står i centrum, med fokus på folk med funktionsnedsättningar. Våra förhoppningar är att kvällen kan få HBTQIA+ personer att bli hoppfulla inför en framtid med mer gemenskap och trygghet. Vi vill även påminna om att HBTQIA+ personer existerar och är viktiga året om, inte bara under Pride. </p>
+        <div id="castlePics">
+          <img src="KalmarSlott1.jpg"/>
+          <img height="400px"src="KalmarSlott2.jpg"/>
+        </div>
       </div>
       <Footer></Footer>
     </>
