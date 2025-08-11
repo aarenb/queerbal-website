0498286ee5a1635d485e5c1e74f2451eed041678
Aaren Bertilsson <ab226gw@student.lnu.se>
2025-08-11 14:11:15 +0200

    Add ticket information

--- src/components/Tickets/Tickets.jsx
+++ src/components/Tickets/Tickets.jsx
@@ -10,7 +10,12 @@ function Tickets() {
       <img id="hangingDecorations" src="hangingDecorations.png"></img>
       <div id="main">
         <h1>Biljetter</h1>
-        <p> Mer information kommer snart!</p>
+        <p> Boka din biljett genom att fylla i <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeL3ZaaxH8XpUyvz0fSS5q6cwjbnAwn_MvsROHlo37n_lLVuA/viewform?usp=header">detta formuläret</a></p>
+        <div id="prices">
+        <h2> Priser </h2>
+        <p> Medlemmar i <a target="_blank" href="https://blimedlem.rfslungdom.se/">RFSL Ungdom Kalmar</a> eller <a target="_blank" href="https://blimedlem.rfsl.se">RFSL Kalmar Län</a>: 250kr</p>
+        <p> Icke medlemmar: 350kr</p>
+        </div>
       </div>
       <Footer></Footer>
     </>
