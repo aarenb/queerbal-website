2f0700a35f6556d3f3bbaeaa140c6546dc5fa6fa
Aaren Bertilsson <ab226gw@student.lnu.se>
2025-09-07 12:46:10 +0200

    Add sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -10,7 +10,15 @@ function Sponsors() {
       <img id="hangingDecorations" src="hangingDecorations.png"></img>
       <div id="main">
         <h1>Sponsorer</h1>
-        <p> Mer information kommer snart!</p>
+        <div id="sponsorsDiv">
+          <img class="sponsorImg" src="301174098_129808743118829_3451087187308102479_n.png" />
+          <img class="sponsorImg" src="epiroc-logo-vector.png" />
+          <img class="sponsorImg" src="HBT-S_Ros_utan_skugga_CMYK_30x30cm.png" />
+          <img class="sponsorImg" src="Kalmar_kommun_vanster_RGB.png" />
+          <img class="sponsorImg" src="kalmarhem_logo_2019_transparent.png" />
+          <img class="sponsorImg" src="logo.webp" />
+          <img class="sponsorImg" src="sodra-logo-green-rgb.png" />
+        </div>
       </div>
       <Footer></Footer>
     </>
