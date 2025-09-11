05b683c67eff948e812b5c14708f32908a41defa
Aaren Bertilsson <ab226gw@student.lnu.se>
2025-09-11 13:07:44 +0200

    Add schedule

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -10,7 +10,9 @@ function Schedule() {
       <img id="hangingDecorations" src="hangingDecorations.png"></img>
       <div id="main">
         <h1>Schema</h1>
-        <p>18:00 - 02:00</p>
+        <p>18:00 | Balen öppnar</p>
+        <p>19:00 - 21:00 | Middag med live musik & andra aktiviteter </p>
+        <p>21:00 - 02:00 | Dans och brädspel </p>
       </div>
       <Footer></Footer>
     </>
